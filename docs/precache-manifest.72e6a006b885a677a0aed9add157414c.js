c8c9bd71c58927a18fa7d84884d99b27e51d4cd4
Joalbert <joalbertgonzalez@gmail.com>
2020-06-21 03:18:15 -0500

    [ADD]🚀 github pages docs added

---
+++ docs/precache-manifest.72e6a006b885a677a0aed9add157414c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ca930e2b558ef6f29757f2344b7278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46213b49fa99f5af9550",
+    "url": "/static/css/main.c192a033.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46213b49fa99f5af9550",
+    "url": "/static/js/main.5c990fd1.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
